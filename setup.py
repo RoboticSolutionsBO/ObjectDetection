c1a6b503d1fd7030d60331e109798d8f16e28f82
Gabriel Chavez <gchavezmardonez@gmail.com>
2019-02-03 22:30:56 -0400

    Update: setup file

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
-print("Hello World!.")
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+        'name': 'objectdetection',
+        'description': 'Application for object detection',
+        'author': 'Robotic Solutions',
+        'version': '0.1',
+        'install_requires': [],
+        'packages': [],
+        'scripts': []
+}
+
+setup(**config)
